1eb35a7fb7c6f0dca75857b8801186d5a3dcb6cd
sahidul-11 <mdsahidulislam161113@gmail.com>
2024-05-20 13:31:33 +0600

    localhoast change

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      // "http://localhost:5173",
+      "http://localhost:5173",
       "https://bolgs-website.web.app",
       "https://bolgs-website.firebaseapp.com",
     ],
